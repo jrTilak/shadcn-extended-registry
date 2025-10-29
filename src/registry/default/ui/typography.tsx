c97136641c1062188c52230d4daf0c0cea3fd257
jrtilak <tilakthapa9898@gmail.com>
2025-10-29 07:43:31 +0545

    add tyopgraphy

---
+++ src/registry/default/ui/typography.tsx
@@ -0,0 +1,64 @@
+import { cn } from "@/lib/utils";
+import React, { ComponentProps } from "react";
+
+const H1 = ({ className, ...props }: ComponentProps<"h2">) => {
+  return (
+    <h1
+      className={cn(
+        "text-4xl font-bold tracking-tight font-heading",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+const H2 = ({ className, ...props }: ComponentProps<"h2">) => {
+  return (
+    <h2
+      className={cn(
+        "text-3xl font-semibold tracking-tight font-heading",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+const H3 = ({ className, ...props }: ComponentProps<"h3">) => {
+  return (
+    <h3
+      className={cn(
+        "text-2xl font-semibold tracking-tight font-heading",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+const H4 = ({ className, ...props }: ComponentProps<"h4">) => {
+  return (
+    <h4
+      className={cn(
+        "text-xl font-medium tracking-tight font-heading",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+const Paragraph = ({ className, ...props }: ComponentProps<"p">) => {
+  return (
+    <p
+      className={cn(
+        "leading-7 text-base text-muted-foreground font-body",
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export { H1, H2, H3, Paragraph, H4 };
